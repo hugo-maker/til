ea01df509892945d53a598506174cacad006ebc3
hugo-maker <hibiki.for.business1@gmail.com>
2020-10-22 21:54:52 +0900

    added new files

---
+++ CLang/Chapter/test15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int *data;
+  int i;
+  int average = 0;
+  int array[10] = {15,78,98,15,98,85,17,35,42,15};
+
+  //ここに注目
+  for (data = array;data != &array[10];data++) {
+    average += *data;
+  }
+
+  printf("%d\n", average / 10);
+  return 0;
+}
