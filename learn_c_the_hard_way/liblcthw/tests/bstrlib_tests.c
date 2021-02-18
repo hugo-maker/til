037267b2a5e022386716cbb8cebbe6a7803bd5c8
hugo-maker <hibiki.for.business1@gmail.com>
2021-02-18 19:30:18 +0900

    Added today's activities

--- learn_c_the_hard_way/liblcthw/tests/bstrlib_tests.c
+++ learn_c_the_hard_way/liblcthw/tests/bstrlib_tests.c
@@ -102,6 +102,8 @@ char * test_bassignblk()
       "Source is NULL, should return BSTR_ERR.");
   mu_assert(bassignblk(string1, str_max, INT_MAX) == BSTR_ERR,
       "Source is too long, should return BSTR_ERR.");
+
+  // OK case
   mu_assert(bassignblk(string2, test_str, length) == BSTR_OK,
       "Should return BSTR_OK.");
   mu_assert(blength(string2) == length, "Length do not much.");
@@ -109,47 +111,201 @@ char * test_bassignblk()
   return NULL;
 }
 
-/*
-char * test_bestory()
+char * test_bdestroy()
 {
+  bstring test_bstr = bfromcstr("test string");
+  bstring null_bstr = bfromcstr(NULL);
+  // invalid bstring
+  mu_assert(bdestroy(NULL) == BSTR_ERR,
+      "Passing NULL, should return BSTR_ERR.");
+  mu_assert(bdestroy(null_bstr) == BSTR_ERR,
+      "Passing null-data bstring, should return BSTR_ERR.");
+
+  // OK case
+  mu_assert(bdestroy(test_bstr) == BSTR_OK,
+      "should return BSTR_OK.");
+
+  return NULL;
 }
 
 char * test_bconcat()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b0 = bfromcstr("one two three");
+  bstring b1 = bfromcstr("four five six");
+
+  int after_concat_length = blength(b0) + blength(b1);
+
+  // invalid destination or source
+  mu_assert(bconcat(NULL, b0) == BSTR_ERR,
+      "Passing NULL destination, should return BSTR_ERR.");
+  mu_assert(bconcat(null_bstr, b0) == BSTR_ERR,
+      "Passing null-data destination, should return BSTR_ERR.");
+  mu_assert(bconcat(b0, NULL) == BSTR_ERR,
+      "Passing NULL source, should return BSTR_ERR.");
+  mu_assert(bconcat(b0, null_bstr) == BSTR_ERR,
+      "Passing null-data source, should return BSTR_ERR.");
+
+  // OK case
+  mu_assert(bconcat(b0, b1) == BSTR_OK, "should return BSTR_OK.");
+
+  mu_assert(blength(b0) == after_concat_length,
+      "Length do not match.");
+
+  return NULL;
 }
 
 char * test_bstricmp()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b0 = bfromcstr("Abc");
+  bstring b1 = bfromcstr("abCD");
+  bstring b2 = bfromcstr("aBcd");
+
+  // invalid bstring
+  mu_assert(bstricmp(NULL, b1) == SHRT_MIN, "Should return SHRT_MIN.");
+  mu_assert(bstricmp(b0, NULL) == SHRT_MIN, "Should return SHRT_MIN.");
+
+  // ?
+  mu_assert(bstricmp(null_bstr, b1) == SHRT_MIN, "Should return SHRT_MIN.");
+  mu_assert(bstricmp(b0, null_bstr) == SHRT_MIN, "Should return SHRT_MIN.");
+
+  // two bstring are equal
+  mu_assert(bstricmp(b1, b2) == BSTR_OK, "Should return BSTR_OK.");
+
+  // two bstring are different
+  mu_assert(bstricmp(b0, b1) == - (char) downcase(b1->data[b0->slen]),
+      "Do not expected return value.");
+  mu_assert(bstricmp(b1, b0) == (char) downcase(b1->data[b0->slen]),
+      "Do not expected return value.");
+    
+  // first extra character is '\0'
+
+  return NULL;
 }
 
 char * test_biseq()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b0 = bfromcstr("abc");
+  bstring b1 = bfromcstr("abc");
+  bstring b2 = bfromcstr("\0");
+  // invalid bstring
+  mu_assert(biseq(NULL, b0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(biseq(b0, NULL) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(biseq(null_bstr, b0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(biseq(b0, null_bstr) == BSTR_ERR, "Should return BSTR_ERR.");
+  // equal
+  mu_assert(biseq(b0, b1) == 1, "These strings are the same.");
+  // differ
+  mu_assert(biseq(b0, b2) == 0, "These strings differ.");
+
+  return NULL;
 }
 
 char * test_binstr()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b1 = bfromcstr("onetwothree");
+  bstring b2 = bfromcstr("two");
+  bstring b3 = bfromcstr("\0");
+
+
+  // invalid bstring
+  mu_assert(binstr(NULL, 0, b2) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(binstr(null_bstr, 0, b2) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(binstr(b1, 0, NULL) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(binstr(b1, 0, null_bstr) == BSTR_ERR, "Should return BSTR_ERR.");
+
+  // case b1->slen == pos
+  int pos = b1->slen;
+  mu_assert(binstr(b1, pos, b3) == pos, "Do not expected return value.");
+  mu_assert(binstr(b1, pos, b2) == BSTR_ERR, "Should return BSTR_ERR.");
+
+  // invalid pos
+  mu_assert(binstr(b1, pos + 1, b2) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(binstr(b1, -1, b2) == BSTR_ERR, "Should return BSTR_ERR.");
+  int wrong_pos = b1->slen - b2->slen + 2;
+  mu_assert(binstr(b1, wrong_pos, b2) == BSTR_ERR, "No space to find such a string.");
+
+  // case b2->slen == 0
+  mu_assert(binstr(b1, 1, b3) == 1, "Do not expected return value.");
+
+  // OK case
+  mu_assert(binstr(b1, 0, b2) == 3, "Do not expected return value.");
+
+  return NULL;
 }
 
-char * test_findreplace()
+char * test_bfindreplace()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b1 = bfromcstr("onetwothree");
+  bstring find = bfromcstr("two");
+  bstring replace = bfromcstr("2");
+
+  mu_assert(bfindreplace(NULL, find, replace, 0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(bfindreplace(b1, NULL, replace, 0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(bfindreplace(b1, find, NULL, 0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(bfindreplace(null_bstr, find, replace, 0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(bfindreplace(b1, null_bstr, replace, 0) == BSTR_ERR, "Should return BSTR_ERR.");
+  mu_assert(bfindreplace(b1, find, null_bstr, 0) == BSTR_ERR, "Should return BSTR_ERR.");
+
+  mu_assert(bfindreplace(b1, find, replace, 0) == BSTR_OK, "Should return BSTR_OK.");
+  return NULL;
 }
 
 char * test_bsplit()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b1 = bfromcstr("onetwothree");
+  // invalid bstring
+  mu_assert(bsplit(NULL, 'a') == NULL, "Passing NULL as bstring, return NULL.");
+  mu_assert(bsplit(null_bstr, 'a') == NULL, "Passing null-string, return NULL.");
+
+  // case splitChar is '\0'
+  struct bstrList * zero_divided_bstrList = bsplit(b1, '\0');
+  bstring b2 = *(zero_divided_bstrList->entry);
+  mu_assert(bstricmp(b1, b2) == 0, "Do not match length.");
+
+  struct bstrList * b1_strList = bsplit(b1, 't');
+  bstring b3 = *(b1_strList->entry);
+  mu_assert(bstricmp(b1, b3) > 0, "Do not expected return value.");
+
+  return NULL;
 }
 
 char * test_bformat()
 {
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b1 = bfromcstr("onetwothree");
+
+  mu_assert(bformat(NULL) == NULL, "Passing NULL, should return NULL.");
+
+  null_bstr = bformat("%s", test_str);
+  mu_assert(blength(null_bstr) == (int)strlen(test_str), "Do not match length.");
+
+  b1 = bformat("%s", test_str);
+  mu_assert(blength(b1) == (int)strlen(test_str), "Do not match length.");
+
+  return NULL;
 }
 
-char * test_bdata()
+char * test_bdata_bchar()
 {
-}
 
-char * test_bchar()
-{
+  bstring null_bstr = bfromcstr(NULL);
+  bstring b1 = bfromcstr("onetwothree");
+
+  mu_assert(bdata(null_bstr) == NULL, "Wrong data from null_bstr.");
+  mu_assert(bdata(b1) == (char *)(b1->data), "Wrong data from b1.");
+
+  mu_assert(bchar(null_bstr, 0) == 0, "Unexpected return value.");
+  mu_assert(bchar(b1, 100) == '\0', "Unexpected return value.");
+  mu_assert(bchar(b1, 4) == b1->data[4], "Unexpected return value.");
+
+  return NULL;
 }
-*/
 
 char * all_tests()
 {
@@ -161,6 +317,15 @@ char * all_tests()
   mu_run_test(test_bassign);
   mu_run_test(test_bassigncstr);
   mu_run_test(test_bassignblk);
+  mu_run_test(test_bdestroy);
+  mu_run_test(test_bconcat);
+  mu_run_test(test_bstricmp);
+  mu_run_test(test_biseq);
+  mu_run_test(test_binstr);
+  mu_run_test(test_bfindreplace);
+  mu_run_test(test_bsplit);
+  mu_run_test(test_bformat);
+  mu_run_test(test_bdata_bchar);
 
   return NULL;
 }
